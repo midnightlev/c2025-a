81d547a78bdfb91668dc7740578b79c0339c4513
midnightlev <2025080906015@std.uestc.edu.cn>
2025-09-15 19:35:59 +0800

    finish p02

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,23 @@
-#include <printf.h>
+#include <stdio.h>
 
 int main() {
-    printf("hello world!\n");
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    // 排除特殊值 0, 1
+    if (num == 1 || num == 0)
+    {
+        printf("Not prime\n");
+        return 0;
+    }
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            printf("Not Prime\n");
+            return 0;
+        }
+    }
+    printf("Prime\n");
     return 0;
 }
