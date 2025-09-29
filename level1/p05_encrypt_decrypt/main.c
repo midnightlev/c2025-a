594933efcbc26ef2197cdd966374fe0c116f47d5
midnightlev <2025080906015@std.uestc.edu.cn>
2025-09-29 20:49:40 +0800

    complete challenge 5

--- level1/p05_encrypt_decrypt/main.c
+++ level1/p05_encrypt_decrypt/main.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
-// to be done
+void encrypt(const char* input, char* output) {
+    const int key = 0b110000111111111111;
 
-// char get_input(char hint[])
-// {
-//     printf("%s", hint);
-//     while (getchar() != '\n')
-//     {
-//
-//     }
-//     return getchar();
-// }
-//
-// int main() {
-//     printf("Input: ");
-//     char input[];
-//     scanf("%s", input);
-//     printf("Output: ");
-//     return 0;
-// }
+    for (int i = 0; i < strlen(input); i++) {
+        output[i] = input[i] ^ key;
+    }
+    output[strlen(input)] = '\0';
+}
+
+void decrypt(const char* input, char* output) {
+    encrypt(input, output);
+}
+
+int main() {
+    char input[500];
+    char encrypted[500];
+    char decrypted[500];
+
+    printf("Input: ");
+    scanf("%499s", input);
+
+    // encrypt
+    encrypt(input, encrypted);
+    printf("Encrypted: %s\n", encrypted);
+
+    // decrypt
+    decrypt(encrypted, decrypted);
+    printf("Decrypted: %s\n", decrypted);
+
+    return 0;
+}
